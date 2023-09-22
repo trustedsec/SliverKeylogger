9a736809b8cb235539e805a2b3281f0e4bd1e14e
stoXe <16467875+IAmStoxe@users.noreply.github.com>
2023-09-22 12:30:38 -0700

    Create parse_keylogger.py
    Log Interpreter Script
    =======================
    
    Description:
    ------------
    This script provides utilities to interpret a textual log of keystrokes and convert it into human-readable text.
    Specifically, it handles special sequences like `[bs]` for backspaces and `[enter]` for newline entries. Arrow keys
    like `[←]` and `[↓]` are recognized but currently not interpreted for cursor movement.
    
    Functions:
    ----------
    - interpret_backspaces(line: str) -> str:
        Interprets and processes backspaces in the given line.
        
    - interpret_enter_keys(line: str) -> str:
        Interprets and processes enter keys, replacing them with newlines.
    
    - process_log_line(line: str) -> str:
        Calls other functions to process a single line of the log. More special sequences can be added and interpreted here.
    
    - interpret_log_string(log_str: str) -> str:
        Processes the entire log string and returns the interpreted text.
    
    Usage:
    ------
    To interpret a log string, simply call the `interpret_log_string` function with the log string as an argument.
    
    Example:
        log_content = "...(log content here)..."
        interpreted_text = interpret_log_string(log_content)
        print(interpreted_text)

---
+++ scripts/parse_keylogger.py
@@ -0,0 +1,75 @@
+def interpret_backspaces(line):
+    """
+    Process the backspaces in the provided line of text.
+    
+    Args:
+    - line (str): The line of text to process.
+    
+    Returns:
+    - str: The processed line of text with backspaces applied.
+    """
+    while '[bs]' in line:
+        index = line.index('[bs]')
+        # Remove the [bs]
+        line = line[:index] + line[index+4:]
+        # Remove the character before [bs] if it exists
+        if index > 0:
+            line = line[:index-1] + line[index:]
+    return line
+
+def interpret_enter_keys(line):
+    """
+    Process the enter keys in the provided line of text.
+    
+    Args:
+    - line (str): The line of text to process.
+    
+    Returns:
+    - str: The processed line of text with enter keys replaced by newlines.
+    """
+    return line.replace('[enter]', '\n')
+
+def process_log_line(line):
+    """
+    Process a line from the log file to interpret special sequences.
+    
+    Args:
+    - line (str): The line of text to process.
+    
+    Returns:
+    - str: The processed line of text.
+    """
+    line = interpret_backspaces(line)
+    line = interpret_enter_keys(line)
+    # Additional interpretations can be added here
+    return line
+
+def interpret_log_file(file_path):
+    """
+    Process the entire log file and return the interpreted text.
+    
+    Args:
+    - file_path (str): The path to the log file.
+    
+    Returns:
+    - str: The interpreted content of the log file.
+    """
+    interpreted_log = []
+    with open(file_path, 'r') as f:
+        for line in f:
+            interpreted_log.append(process_log_line(line))
+    return ''.join(interpreted_log)
+
+def interpret_log_string(log_str):
+    """
+    Process the entire log string and return the interpreted text.
+    
+    Args:
+    - log_str (str): The log content as a string.
+    
+    Returns:
+    - str: The interpreted content of the log.
+    """
+    lines = log_str.split('\n')
+    interpreted_log = [process_log_line(line) for line in lines]
+    return '\n'.join(interpreted_log)
